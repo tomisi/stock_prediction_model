de91e7a8bde29f621343a2ed3fb169818aa78067
T O M I <65241457+tomisi@users.noreply.github.com>
2023-12-15 13:21:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from prophet import Prophet
-from prophet.plot import plot_plotly
-from plotly import graph_objs as go
+from prophet import Prophet  # Changed from 'prophet'
+from prophet.plot import plot_plotly  # Changed from 'prophet.plot'
+import plotly.graph_objs as go  # Changed from 'plotly'
 
 # Setting constants
 START = "2015-01-01"
@@ -23,7 +23,7 @@ n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
 # Caching function to load data
-@st.cache_data
+@st.cache(allow_output_mutation=True)  # Changed from '@st.cache_data'
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
@@ -40,16 +40,16 @@ st.write(data.tail())
 
 # Plot raw data
 def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.update_layout(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)  # Changed from 'layout.update'
+    st.plotly_chart(fig)
+
 plot_raw_data()
 
 # Predict forecast with Prophet.
-df_train = data[['Date','Close']]
+df_train = data[['Date', 'Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
